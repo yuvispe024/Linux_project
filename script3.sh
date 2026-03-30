d20d91b91679b474f806b0133e5946657b0bfac2
yuvispe024 <yuvispe024@gmail.com>
2026-03-30 21:54:42 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script 3: Disk and Permission Auditor
-# Author: Ankshit | Course: Open Source Software
+# Author: Yuvraj singh yadav | Course: Open Source Software
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
